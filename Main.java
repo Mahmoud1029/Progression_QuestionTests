4f944613444667077299a251fa4a28fc6f3407c3
Mahmoud1029 <119633277+Mahmoud1029@users.noreply.github.com>
2025-02-21 12:06:32 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+      public class Main {
+      //+VISIBLE
+        public static void main(String[] args) {
+          Scanner input = new Scanner( System.in );
+          String variable = input.nextLine();
+
+
+          //+TODO
+    
+
+          //-TODO
+
+          }
+          //-VISIBLE
+      }
